a88de129ac730f550a1023ee4f9f0ac2ac436aba
lex0819 <lexmolnar@gmail.com>
2024-06-09 18:08:01 +0300

    HomeView

--- src/components/AuthorsList.vue
+++ src/components/AuthorsList.vue
@@ -14,11 +14,19 @@ const activeAuthor = defineModel();
 <template>
     <div>
         <div v-if="authors">
-            <h2 class="text-center text-teal-700 text-3xl mb-6">
-                Choose one author
-            </h2>
-            <select name="author" id="authors" v-model="activeAuthor">
-                <option value="">all authors</option>
+            <label
+                for="author"
+                class="block text-center text-teal-700 text-3xl mb-6"
+                >Choose one author</label
+            >
+            <select
+                id="author"
+                name="author"
+                title="author"
+                v-model="activeAuthor"
+                class="outline-none border border-teal-700 text-lg rounded-lg focus:ring-teal-700 focus:border-teal-700 block p-3"
+            >
+                <option selected value="">all authors</option>
                 <option
                     v-for="author in authors"
                     :key="author.id"
@@ -27,6 +35,16 @@ const activeAuthor = defineModel();
                     {{ author.name }}
                 </option>
             </select>
+            <!-- <select name="author" id="author" v-model="activeAuthor">
+                <option value="">all authors</option>
+                <option
+                    v-for="author in authors"
+                    :key="author.id"
+                    :value="author.id"
+                >
+                    {{ author.name }}
+                </option>
+            </select> -->
             <p class="italic mb-4">
                 Chosen user:
                 <span class="text-teal-700">{{ activeAuthor }}</span>
